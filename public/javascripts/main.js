ab5c70cb7cfaa8ca121a8186899eb51cb0d447d1
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-11-10 10:08:42 -0800

    Detect being on a hole.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,17 +10,18 @@ $(function() {
     rows: 10,
     cols: 10,
     getSquareWidth: function() {
-        return maze.element.width() / maze.cols;
+        return Math.min(maze.element.width(), maze.element.height()) / maze.cols;
     },
     getSquareHeigth: function() {
-        return maze.element.height() / maze.rows;
+        return Math.min(maze.element.width(), maze.element.height()) / maze.rows;
     }
   };
   ball = {
     x: 0.5,
     y: 0.5,
     vx: 0,
-    vy: 0
+    vy: 0,
+    r: 0.5
   };
   var walls = [
     { sx: 0, sy: 0, dx: 10, dy: 0 }, // top edge
@@ -28,23 +29,31 @@ $(function() {
     { sx: 10, sy: 0, dx: 10, dy: 10 }, // right edge
     { sx: 0, sy: 0, dx: 0, dy: 10 } // left edge
   ];
+  var holes = [
+    { x: 3.5, y: 1.5, r: 0.5 } 
+  ];
   var frame = 0;
   function update() {
     $('#status').html('Frame: ' + frame + ', width= ' + maze.getSquareWidth());
     if (!ball.element) {
       ball.element = $('<div class="ball" />');
-      ball.width = (maze.getSquareWidth() / 2);
-      ball.height = (maze.getSquareHeigth() / 2);
+      ball.width = (maze.getSquareWidth() * ball.r);
+      ball.height = (maze.getSquareHeigth() * ball.r);
       ball.element.css('width', ball.width + 'px');
       ball.element.css('height', ball.height + 'px');
       $('body').append(ball.element);
     }
-    ball.vx += thresolded(Math.sin(leftRightAngle)/10.0);
-    ball.vy += thresolded(Math.sin(frontBackAngle)/10.0);
-    ball.vx = thresolded(ball.vx * 0.85);
-    ball.vy = thresolded(ball.vy * 0.85);
-    ball.x += thresolded(ball.vx);
-    ball.y += thresolded(ball.vy);
+    ball.vx += thresholded(Math.sin(leftRightAngle)/10.0);
+    ball.vy += thresholded(Math.sin(frontBackAngle)/10.0);
+    ball.vx = thresholded(ball.vx * 0.85);
+    ball.vy = thresholded(ball.vy * 0.85);
+    for (var i = 0; i < holes.length; i++) {
+        checkBallHole(ball, holes[i], function(position) {
+            console.log(position);
+        });
+    }
+    ball.x += ball.vx;
+    ball.y += ball.vy;
     ball.element.css('left', (ball.x * maze.getSquareWidth() + 30 - ball.width / 2.0) + 'px');
     ball.element.css('top', (ball.y * maze.getSquareHeigth() + 30 - ball.height / 2.0) + 'px');
     frame++;
